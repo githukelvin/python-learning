4f375b5f41deeec38b96ed1ae951d104e90434e7
githukelvin <kelvingithu019@gmail.com>
2023-07-21 19:30:19 +0300

    day 6

--- 100 days of code/Functions/main.py
+++ 100 days of code/Functions/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+print("hello")
+num_char = len("hello")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(num_char)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def my_function():
+    print("hello")
+    print("Bye")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+my_function()
